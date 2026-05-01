e154af7e84ac8c730be87095a6d60e0222edff61
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-05-01 22:07:09 +0700

    test(noboil): cover root init --help

---
+++ lib/noboil/src/__tests__/init.test.ts
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+import { describe, expect, test } from 'bun:test'
+import { init } from '../init'
+describe('init --help', () => {
+  test('prints usage and returns', async () => {
+    const orig = console.log
+    console.log = () => undefined
+    try {
+      await init(['--help'])
+    } finally {
+      console.log = orig
+    }
+    expect(true).toBe(true)
+  })
+})
