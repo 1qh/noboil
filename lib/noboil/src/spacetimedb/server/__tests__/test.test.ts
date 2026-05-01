52739b6427e3489e66a957ffbaca6b0cdb8fbe7d
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-05-01 21:01:28 +0700

    test(noboil): cover stdb server/test isTestMode

---
+++ lib/noboil/src/spacetimedb/server/__tests__/test.test.ts
@@ -0,0 +1,21 @@
+/** biome-ignore-all lint/nursery/noUndeclaredEnvVars: test env */
+import { afterAll, beforeAll, describe, expect, test } from 'bun:test'
+import { isTestMode } from '../test'
+describe('stdb server/test isTestMode', () => {
+  const orig = process.env.SPACETIMEDB_TEST_MODE
+  beforeAll(() => {
+    process.env.SPACETIMEDB_TEST_MODE = 'true'
+  })
+  afterAll(() => {
+    if (orig === undefined) delete process.env.SPACETIMEDB_TEST_MODE
+    else process.env.SPACETIMEDB_TEST_MODE = orig
+  })
+  test('returns true when env var is "true"', () => {
+    expect(isTestMode()).toBe(true)
+  })
+  test('returns false when env var unset', () => {
+    delete process.env.SPACETIMEDB_TEST_MODE
+    expect(isTestMode()).toBe(false)
+    process.env.SPACETIMEDB_TEST_MODE = 'true'
+  })
+})
