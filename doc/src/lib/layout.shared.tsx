6f89feed440bd26de565c85662a70d0545e9b06d
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-05-08 19:11:54 +0700

    chore: sync repo conventions

---
+++ doc/src/lib/layout.shared.tsx
@@ -0,0 +1,7 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared'
+export const baseOptions = (): BaseLayoutProps => ({
+  githubUrl: 'https://github.com/1qh/noboil',
+  nav: {
+    title: 'docs'
+  }
+})
