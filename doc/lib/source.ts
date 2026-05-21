2cbb65e84b9a07de83590886f8f07423cfd6cb71
Lai Quang Huy <laiquanghuy24122001@gmail.com>
2026-05-21 10:37:25 +0700

    fix(doc): migrate to collections/* fumadocs-mdx convention

--- doc/lib/source.ts
+++ doc/lib/source.ts
@@ -1,10 +1,7 @@
+import { docs } from 'collections/server'
 import { loader } from 'fumadocs-core/source'
-import { docs } from 'fumadocs-mdx:collections/server'
 
-interface FumadocsCollection {
-  toFumadocsSource: () => Parameters<typeof loader>[0]['source']
-}
 export const source = loader({
   baseUrl: '/docs',
-  source: (docs as FumadocsCollection).toFumadocsSource()
+  source: docs.toFumadocsSource()
 })
